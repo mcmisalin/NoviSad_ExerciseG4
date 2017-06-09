f75a0bbb4a8a7de40aaaf8483368ebdbef849c5f
somename <someemail>
2017-06-09 21:13:13 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -104,9 +104,19 @@ public class TestBowling {
 	}
 	
 	@Test
-	public void testOpisPartije(){
+	public void testOpisPartije()throws BowlingException{
 		bg=new BowlingGame();
 		bg.addFrame(new Frame(1,4));
+		bg.addFrame(new Frame(4,5));
+		bg.addFrame(new Frame(6,4));
+		bg.addFrame(new Frame(5,5));
+		bg.addFrame(new Frame(10,0));
+		bg.addFrame(new Frame(0,1));
+		bg.addFrame(new Frame(7,3));
+		bg.addFrame(new Frame(6,4));
+		bg.addFrame(new Frame(10,0));
+		bg.addFrame(new Frame(2,8));
+		bg.setBonus(6, 0);
 		
 	}
 	
