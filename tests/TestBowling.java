e7572ddd2345c54216fdabbd29311d065692cfdb
somename <someemail>
2017-06-09 20:02:53 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -6,15 +6,19 @@ public class TestBowling {
 
 	Frame frame;
 	@Test
-	public void testFrameContrs() {
+	public void testFrameContrs() throws BowlingException{
 		frame= new Frame(2,2);
 		assertNotNull(frame);
 	}
 	
 	@Test
-	public void testSimpleScore(){
+	public void testSimpleScore() throws BowlingException{
 		frame = new Frame(2,2);
 		assertEquals(4,frame.score());
 	}
-
+	
+	@Test(expected=BowlingException.class)
+	public void testWhenThrowMoreThen10(){
+		frame = new Frame()
+	}
 }
