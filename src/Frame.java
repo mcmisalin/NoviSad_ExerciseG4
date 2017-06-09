53af9d5eda73de5df4ec259fa27756d69791e8de
somename <someemail>
2017-06-09 20:10:16 +0200

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -29,7 +29,9 @@ public class Frame {
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
 		if(this.firstThrow==10)
-		return false;
+			return true;
+		else
+			return false;
 	}
 	
 	//return whether a frame is a spare or not
